2f04243d4984382234b45fd7a5b5f81cd8f69eaa
Geoffroy Van Elsuve <geoffroy-externe.van-elsuve@enedis.fr>
2018-01-24 15:22:39 +0100

    Transform internal representation to two-dimensionnal Array

--- MaximumPath/src/main/java/fr/esiea/inf3036/MaximumPath.java
+++ MaximumPath/src/main/java/fr/esiea/inf3036/MaximumPath.java
@@ -1,14 +1,62 @@
 package fr.esiea.inf3036;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class MaximumPath {
-    private final String triangle;
+    private final int[][] triangle;
+    private final int nbLine;
 
     public MaximumPath(String triangle) {
-        this.triangle = triangle;
+        String[] element = triangle.split(" ");
+        // nous avons tous les éléments.
+        // On calcule le nombre de ligne
+        nbLine = (int) Math.sqrt(element.length)+1;
+        //System.out.println("triangle nbLine = " + nbLine);
+        this.triangle = new int[nbLine][nbLine];
+
+        int elementIndex = 0;
+        // Move over array lines
+        for(int lineIndex = 0; lineIndex<nbLine; lineIndex++) {
+            //System.out.print(String.format("Line %d contains ", lineIndex));
+            // move over array columns
+            for(int columnIndex = 0 ; columnIndex<lineIndex+1; columnIndex++) {
+                //System.out.print(element[elementIndex] + " ");
+                this.triangle[lineIndex][columnIndex] = Integer.parseInt(element[elementIndex++]);
+            }
+            //System.out.println();
+        }
     }
 
     public String getPath() {
+        // Ici la représentation du triangle ne nous aides pas vraiement à résoudre le problème.
+        List<Integer> path = maxPath(0, 0);
+        return path.stream().map(String::valueOf).collect(Collectors.joining(" "));
+    }
 
-        return null;
+    private List<Integer> maxPath(int lineIndex, int position) {
+        //System.out.println("Processing element "+triangle[lineIndex][position]);
+        List<Integer> bestPossiblePath = new ArrayList();
+        bestPossiblePath.add(triangle[lineIndex][position]);
+        // Detect leaf, there are no more line. return no path
+        if(lineIndex>nbLine-2) {
+            return bestPossiblePath;
+        }
+
+        //System.out.println("Possible path are "+triangle[lineIndex+1][position] + ", "+triangle[lineIndex+1][position+1]);
+        List<Integer> path1 = maxPath(lineIndex+1, position);
+        List<Integer> path2 = maxPath(lineIndex+1, position+1);
+        if(path1.stream().mapToInt(Integer::intValue).sum() > path2.stream().mapToInt(Integer::intValue).sum()) {
+            bestPossiblePath.addAll(path1);
+        } else {
+            bestPossiblePath.addAll(path2);
+        }
+
+        return bestPossiblePath;
     }
 }
